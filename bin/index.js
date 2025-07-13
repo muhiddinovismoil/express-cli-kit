ada965a17fe2cf1104fb9c6703ace647b2a5e87e
Ismoil Muhiddinov <muhiddinovismoil2@gmail.com>
2025-07-13 23:11:00 +0500

    new package were installed and integrated to project

--- bin/index.js
+++ bin/index.js
@@ -1,14 +1,21 @@
 #!/usr/bin/env node
+import { Command } from "commander";
 import { generate } from "../src/commands/generate.js";
 
-const [, , cmd, type, name] = process.argv;
+const program = new Command();
 
-if (cmd === "g" || cmd === "generate") {
-    if (!type || !name) {
-        console.log("❌ Usage: express-cli g <type> <name>");
-        process.exit(1);
-    }
-    generate(type, name);
-} else {
-    console.log("❌ Unknown command.");
-}
+program
+    .name("express-cli")
+    .description("Express CLI tool to generate project components")
+    .version("1.0.0");
+
+program
+    .command("generate")
+    .alias("g")
+    .argument("<type>", "Type of component (controller, route, service, etc.)")
+    .argument("<name>", "Name of the component")
+    .action((type, name) => {
+        generate(type, name);
+    });
+
+program.parse();
