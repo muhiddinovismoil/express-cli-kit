836dae1daa8635d13b060e65b05bf08ea85afb85
Ismoil Muhiddinov <muhiddinovismoil2@gmail.com>
2025-07-13 22:56:58 +0500

    first commit

---
+++ src/commands/generate.js
@@ -0,0 +1,41 @@
+import fs from "fs";
+import path from "path";
+import ejs from "ejs";
+import ora from "ora";
+import chalk from "chalk";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export async function generate(type, name) {
+    const spinner = ora(`Generating ${type} "${name}"...`).start();
+
+    try {
+        const templatePath = path.join(__dirname, `../templates/${type}.ejs`);
+        const outputFileName = `${name}.${type}.js`;
+        const outputPath = path.join(process.cwd(), outputFileName);
+
+        if (!fs.existsSync(templatePath)) {
+            spinner.fail(chalk.red(`Template for '${type}' not found.`));
+            return;
+        }
+
+        if (fs.existsSync(outputPath)) {
+            spinner.fail(chalk.red(`File ${outputFileName} already exists.`));
+            return;
+        }
+
+        const capitalizedName = name.charAt(0).toUpperCase() + name.slice(1);
+
+        const content = await ejs.renderFile(templatePath, {
+            name,
+            capitalizedName,
+        });
+        fs.writeFileSync(outputPath, content);
+
+        spinner.succeed(chalk.green(`${type} "${name}" created successfully.`));
+    } catch (err) {
+        spinner.fail(chalk.red(`Error: ${err.message}`));
+    }
+}
